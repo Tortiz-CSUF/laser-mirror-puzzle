811279b73b3bff16c35e4104f8b05f68e2451302
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-16 02:01:55 +0000

    laser-mirror-puzzle-Interim_4

--- Scripts/game_board.gd
+++ Scripts/game_board.gd
@@ -1,11 +1,39 @@
 extends Node2D
 
+# vars
+var grid_width: int = 8
+var grid_height: int = 8
+var grid: Array = [] 			# Pieces data
 
-# Called when the node enters the scene tree for the first time.
+
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_init_grid()
+	_draw_tiles()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _init_grid():
+	grid = []
+	for x in range(grid_width):
+		var col := []
+		for y in range(grid_height):
+			col.append(GameData.PieceType.EMPTY)
+		grid.append(col)
+
+func _draw_tiles():
+	
+	
+func _grid_to_world():
+	
+	
+func _cell_center():
+	
+	
+func _world_to_grid():
+	
+	
+func _is_valid_cell():
+	
+	
+func _draw():
