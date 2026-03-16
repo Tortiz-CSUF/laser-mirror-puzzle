1b4e6c4e917bca88e45cb3396474eb53c6c25771
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-16 01:24:28 +0000

    'laser-mirror-puzzle-Interim_%02d' 1

---
+++ Scripts/test.gd
@@ -0,0 +1,14 @@
+extends Node
+## this is a test to see if the git auto commit bash works. 
+const this := 4.0
+const THAT := 5.0
+
+## if this is commited then it works 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
