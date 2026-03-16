76018633b7c4526780f45bcda688a08114bf7c1c
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-16 01:46:51 +0000

    laser-mirror-puzzle-Interim_2

--- Scripts/GameData.gd
+++ Scripts/GameData.gd
@@ -1,11 +1,46 @@
 extends Node
 
+## Grid Settings
+const CELL_SIZE := 64
+const GRID_OFFSET := Vector2(160, 40)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+## Piece Types
+enum PieceType {
+	EMPTY,
+	LASER,
+	GOAL,
+	MIRROR_STATIC_SINGLE,
+	MIRROR_STATIC_DOUBLE,
+	MIRROR_ROTATE_SINGLE,
+	MIRROR_ROTATE_DOUBLE,
+	MIRROR_SLIDE_H,
+	MIRROR_SLLIDE_V,
+	BARRIER,
+	HAZARD,
+	BOMB
+	
+}
 
+## Mirror Orientation: Direction of reflection
+enum MirrorDIr {
+	NE,
+	NW,
+	SE,
+	SW
+	
+}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+enum Dir {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+	
+}
+
+enum LevelStatus {
+	LOCKED, 
+	INCOMPLETE,
+	COMLETE
+	
+}
