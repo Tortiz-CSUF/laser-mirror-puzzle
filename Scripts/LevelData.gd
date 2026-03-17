0148a37efb9e43c643cf7cc90d05e8adf6192bd9
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-17 22:10:54 +0000

    laser-mirror-puzzle-Interim_3

--- Scripts/LevelData.gd
+++ Scripts/LevelData.gd
@@ -33,8 +33,85 @@ func get_level(level_num: int) -> Dictionary:
 	
 	
 var levels: Array = [
+	# Lvl 1: 1 static double mirror
+	{
+		"width": 6,
+		"height": 6,
+		"pieces": [
+			{"x": 0, "y": 2, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
+			{"x": 3, "y": 2, "type": "MIRROR_STATIC_DOUBLE", "mirror_dir": "NE", "double_sided": true},
+			{"x": 3, "y": 5, "type": "GOAL", "color_index": 0},		
+		],
+		"inventory": []
+	},
 	
+	# Lvl 2: 1 static single mirror
+	{
+		"width": 6,
+		"height": 6,
+		"pieces": [
+			{"x": 0, "y": 1, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
+			{"x": 4, "y": 1, "type": "MIRROR_STATIC_SINGLE", "mirror_dir": "NE", "double_sided": false},
+			{"x": 4, "y": 4, "type": "GOAL", "color_index": 0},		
+		],
+		"inventory": []
+	},
 	
+	# lVL 3: ROTATABLE MIRROR
+	{
+		"width": 6,
+		"height": 6,
+		"pieces": [
+			{"x": 0, "y": 2, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
+			{"x": 3, "y": 3, "type": "MIRROR_ROTATE_SINGLE", "mirror_dir": "NW", "double_sided": false},
+			{"x": 3, "y": 0, "type": "GOAL", "color_index": 0},		
+		],
+		"inventory": []
+	},
+	
+	# lVL 4: ROTATABLE MIRROR AND BARRIER
+	{
+		"width": 7,
+		"height": 7,
+		"pieces": [
+			{"x": 0, "y": 3, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
+			{"x": 6, "y": 3, "type": "BARRIER"},
+			{"x": 3, "y": 3, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},
+			{"x": 3, "y": 0, "type": "MIRROR_STATIC_DOUBLE", "mirror_dir": "NW", "double_sided": true},
+			{"x": 6, "y": 0, "type": "GOAL", "color_index": 0},		
+		],
+		"inventory": []
+	},
+	
+	# lVL 5: HORIZONTAL SLIDE MIRROR
+	{
+		"width": 7,
+		"height": 7,
+		"pieces": [
+			{"x": 0, "y": 3, "type": "LASER", "laser_dir": "DOWN", "color_index": 0},
+			{"x": 0, "y": 3, "type": "MIRROR_SLIDE_H", "mirror_dir": "NW", "double_sided": true, "slide_axis": "h", "slide_min": 0, "slide_max": 4},
+			{"x": 3, "y": 6, "type": "GOAL", "color_index": 0},
+			{"x": 6, "y": 3, "type": "MIRROR_STATIC_DOUBLE", "mirror_dir": "NE", "double_sided": true},		
+		],
+		"inventory": []
+	},
+	
+	# Lvl 6: vertical slide mirror and hazard
+	{
+		"width": 7,
+		"height": 7,
+		"pieces": [
+			{"x": 0, "y": 3, "type": "LASER", "laser_dir": "DOWN", "color_index": 0},
+			{"x": 3, "y": 3, "type": "MIRROR_STATIC_DOUBLE", "mirror_dir": "NE", "double_sided": true},	
+			{"x": 3, "y": 5, "type": "MIRROR_SLIDE_V", "mirror_dir": "NW", "double_sided": true, "slide_axis": "v", "slide_min": 4, "slide_max": 6},
+			{"x": 4, "y": 5, "type": "HAZARD"},
+			{"x": 6, "y": 5, "type": "GOAL", "color_index": 0},
+				
+		],
+		"inventory": []
+	},
+	
+	# Lvl 7: Invetory 
 	
 	
 	
