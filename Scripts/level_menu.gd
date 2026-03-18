5842b80847bf78d0c7a554c362236c7f693eb6c0
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-18 00:53:33 +0000

    laser-mirror-puzzle-Interim_5

--- Scripts/level_menu.gd
+++ Scripts/level_menu.gd
@@ -1,11 +1,19 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+	
+	
+func _build_level_buttons():
+	
+	
+	
+func _on_level_select(level_num: int):
+	LevelData.current_level = level_num
+	get_tree().change_scene_to_file("res://Scenes/game_board.tscn")
+	
+
+func _on_back():
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
