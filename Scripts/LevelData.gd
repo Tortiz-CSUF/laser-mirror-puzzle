1613259cf21d60ea5be7f8447b52b7dc3fff5aa7
Tortiz-CSUF <thomasortiz@csu.fullerton.edu>
2026-03-18 01:56:49 +0000

    laser-mirror-puzzle-Interim_7

--- Scripts/LevelData.gd
+++ Scripts/LevelData.gd
@@ -115,11 +115,11 @@ var levels: Array = [
 		"width": 7,
 		"height": 7,
 		"pieces": [
-			{"x": 0, "y": 3, "type": "LASER", "laser_dir": "DOWN", "color_index": 0},
+			{"x": 0, "y": 3, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
 			{"x": 3, "y": 3, "type": "MIRROR_STATIC_DOUBLE", "mirror_dir": "NE", "double_sided": true},	
-			{"x": 3, "y": 5, "type": "MIRROR_SLIDE_V", "mirror_dir": "NW", "double_sided": true, "slide_axis": "v", "slide_min": 4, "slide_max": 6},
-			{"x": 4, "y": 5, "type": "HAZARD"},
-			{"x": 6, "y": 5, "type": "GOAL", "color_index": 0},
+			{"x": 3, "y": 6, "type": "MIRROR_SLIDE_V", "mirror_dir": "NE", "double_sided": true, "slide_axis": "v", "slide_min": 4, "slide_max": 6},
+			{"x": 5, "y": 4, "type": "HAZARD"},
+			{"x": 6, "y": 4, "type": "GOAL", "color_index": 0},
 				
 		],
 		"inventory": []
@@ -131,11 +131,11 @@ var levels: Array = [
 		"height": 7,
 		"pieces": [
 			{"x": 0, "y": 3, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
-			{"x": 6, "y":05, "type": "GOAL", "color_index": 0},
-			{"x": 4, "y": 3, "type": "BARRIER"},
+			{"x": 3, "y": 3, "type": "BARRIER"},
+			{"x": 6, "y": 0, "type": "GOAL", "color_index": 0},
 		],
 		"inventory": [
-			{"type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NE", "double_sided": true},
+			{"type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},
 			{"type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},
 		]
 	},
@@ -146,9 +146,9 @@ var levels: Array = [
 		"height": 7,
 		"pieces": [
 			{"x": 0, "y": 3, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
-			{"x": 3, "y": 3, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NE", "double_sided": true},	
-			{"x": 3, "y": 0, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},
-			{"x": 7, "y": 3, "type": "BOMB"},
+			{"x": 4, "y": 3, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},	
+			{"x": 4, "y": 0, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NE", "double_sided": true},
+			{"x": 4, "y": 6, "type": "BOMB"},
 			{"x": 7, "y": 0, "type": "GOAL", "color_index": 0},
 				
 		],
@@ -162,10 +162,10 @@ var levels: Array = [
 		"pieces": [
 			{"x": 0, "y": 1, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
 			{"x": 0, "y": 6, "type": "LASER", "laser_dir": "RIGHT", "color_index": 1},
-			{"x": 4, "y": 1, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NE", "double_sided": true},	
-			{"x": 4, "y": 6, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},
-			{"x": 7, "y": 4, "type": "GOAL", "color_index": 0},
-			{"x": 4, "y": 4, "type": "GOAL", "color_index": 1},
+			{"x": 4, "y": 1, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},	
+			{"x": 5, "y": 6, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NE", "double_sided": true},
+			{"x": 4, "y": 7, "type": "GOAL", "color_index": 0},
+			{"x": 5, "y": 0, "type": "GOAL", "color_index": 1},
 			{"x": 6, "y": 1, "type": "BOMB"},
 				
 		],
@@ -177,9 +177,8 @@ var levels: Array = [
 		"width": 8,
 		"height": 8,
 		"pieces": [
-			{"x": 0, "y": 0, "type": "LASER", "laser_dir": "DOWN", "color_index": 0},
-			{"x": 0, "y": 7, "type": "LASER", "laser_dir": "RIGHT", "color_index": 1},
-			{"x": 0, "y": 4, "type": "MIRROR_STATIC_SINGLE", "mirror_dir": "NW", "double_sided": false},	
+			{"x": 0, "y": 4, "type": "LASER", "laser_dir": "RIGHT", "color_index": 0},
+			{"x": 3, "y": 4, "type": "MIRROR_ROTATE_DOUBLE", "mirror_dir": "NW", "double_sided": true},	
 			{"x": 3, "y": 0, "type": "MIRROR_SLIDE_H", "mirror_dir": "NE", "double_sided": true, "slide_axis": "h", "slide_min": 1, "slide_max": 5},
 			{"x": 5, "y": 4, "type": "MIRROR_SLIDE_V", "mirror_dir": "NW", "double_sided": true, "slide_axis": "v", "slide_min": 3, "slide_max": 7},
 			{"x": 3, "y": 7, "type": "MIRROR_ROTATE_SINGLE", "mirror_dir": "NW", "double_sided": false},	
